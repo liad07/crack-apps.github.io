6179709af562d73a49c8a5dc526912677d17dbec
liadk07 <91607695+liad07@users.noreply.github.com>
2023-02-17 10:42:02 +0200

    Update finger.js

--- finger.js
+++ finger.js
@@ -15,39 +15,35 @@
 
     const response = await fetch('https://ipapi.co/json/');
     const data = await response.json();
-    console.log(data.city);
     fetch('https://www.hebcal.com/shabbat?cfg=i2&city='+data.city+'&b=18&M=on&lg=he-x-NoNikud&tgt=_top')
         .then(response => response.text())
         .then(data => {
             const parser = new DOMParser();
             const doc = parser.parseFromString(data, 'text/html');
             const firstStrong = doc.querySelectorAll('strong');
-            console.log(firstStrong[0].textContent+"\n"+firstStrong[1].textContent);
             let cnisahour = firstStrong[0].textContent.split(":")[0];
             const cnisaminute = firstStrong[0].textContent.split(":")[1];
 
             const outhour = firstStrong[1].textContent.split(":")[0];
             const outminute = firstStrong[1].textContent.split(":")[1];
 
-            console.log(cnisahour+":"+cnisaminute)
-            console.log(cnisahour+":"+cnisaminute)
             const now = new Date();
             let currentHour = now.getHours();
             const currentMinute = now.getMinutes();
             const day=now.getDay()
             if (currentHour < cnisahour || (currentHour === cnisahour && cnisaminute < currentMinute&&day==5)) {
-console.log("עדיין לא נכנסה שבת ")
-                }
+var d=false;
+            }
      else {
              if (currentHour > outhour || (currentHour === outhour && currentMinute >= outminute&&day==6)){
-                    console.log("יצאה שבת")
+                 var d=false
                 }
                 else {
                     document.writeln("יהודי יקר,\n" +
                         "\n" +
-                        "האתר בו הנך מבקר שומר שבת וחג, ולכן הגלישה בו אינה מתאפשרת בזמן זה.\n" +
+                        "האתר בו הנך מבקר שומר שבת , ולכן הגלישה בו אינה מתאפשרת שבת קודש .\n" +
                         "\n" +
-                        "'כִּי אֶשְׁמְרָה שַׁבָּת אֵל יִשְׁמְרֵנִי אוֹת הִיא לְעוֹלְמֵי עַד בֵּינוֹ וּבֵינִי\n'");
+                        "'כִּי אֶשְׁמְרָה שַׁבָּת אֵל יִשְׁמְרֵנִי אוֹת הִיא לְעוֹלְמֵי עַד בֵּינוֹ וּבֵינִי'");
                 }
             }
 
